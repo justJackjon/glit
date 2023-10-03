ee07cdc677d81d5689f17a6eb68cd308794c2433
Jackjon Butcher <jackjon@justjackjon.dev>
2023-10-03 17:32:08 +0100

    :recycle: (arg_parsing): Refactor arg parsing methods

--- modules/arg_parsing.sh
+++ modules/arg_parsing.sh
@@ -41,11 +41,7 @@ parse_arg() {
             display_help
             ;;
         -t|--type)
-            if is_missing_value "$next_arg"; then
-                print error "The option $current_arg requires a value."
-
-                exit $EXIT_MISSING_VALUE_FOR_OPTION
-            fi
+            check_opt_missing_value "$current_arg" "$next_arg"
 
             if ! [[ "${ACCEPTABLE_TYPE_ARGS[*]}" =~ $next_arg ]]; then
                 local formatted_args=$(echo "${ACCEPTABLE_TYPE_ARGS[@]}" | sed -E "s/([^ ]+)/'\1',/g; s/,\$//")
@@ -57,18 +53,14 @@ parse_arg() {
 
             VOLUME_TYPE="$next_arg"
 
-            return 1  # Indicate that two arguments have been consumed
+            return $TWO_ARGS_CONSUMED
             ;;
         -V|--volume)
-            if is_missing_value "$next_arg"; then
-                print error "The option $current_arg requires a value."
-
-                exit $EXIT_MISSING_VALUE_FOR_OPTION
-            fi
+            check_opt_missing_value "$current_arg" "$next_arg"
 
             VOLUME_NAME="$next_arg"
 
-            return 1  # Indicate that two arguments have been consumed
+            return $TWO_ARGS_CONSUMED
             ;;
         -y|--yes)
             AUTO_CONFIRM=1
@@ -90,28 +82,30 @@ parse_arg() {
             ;;
     esac
 
-    return 0  # Indicate that only one argument has been consumed
+    return $ONE_ARG_CONSUMED
 }
 
 parse_args() {
     local arguments=("$@")
-    local consume_next=0
+    local skip_current_arg=0
     local total_arguments=${#arguments[@]}
 
     for (( i=0; i<total_arguments; i++ )); do
-        local current_arg="${arguments[$i]}"
-        local next_arg="${arguments[$((i+1))]:-}"
 
-        if (( consume_next )); then
-            consume_next=0
+        if (( skip_current_arg )); then
+            skip_current_arg=0
             continue
         fi
 
+        local current_arg="${arguments[$i]}"
+        local next_arg="${arguments[$((i+1))]:-}"
+
         if (( i+1 < total_arguments )); then
             parse_arg "$current_arg" "$next_arg"
-            consume_next=$?  # 0 if only one argument was consumed, 1 if two arguments were consumed
+            skip_current_arg=$?  # 0 if only one argument was consumed, 1 if two arguments were consumed
         else
             parse_arg "$current_arg"
         fi
+
     done
 }
