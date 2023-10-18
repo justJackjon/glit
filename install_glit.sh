67e834c1191ac5c85e7abb05c5edc63eb2ed483b
Jackjon Butcher <jackjon@justjackjon.dev>
2023-10-18 17:29:34 +0100

    :hammer: (install_glit): Improve dependency checking logic

--- install_glit.sh
+++ install_glit.sh
@@ -7,9 +7,88 @@ set -e
 INSTALL_MODE="remote"
 LOCAL_PATH="$(dirname "$(pwd)")"
 VERSION="latest"
+GH_API_ENDPOINT="https://api.github.com/repos/justJackjon/glit"
+GLIT_REPO_URL="https://github.com/justJackjon/glit"
+GLIT_DIR="/opt/glit"
+SYMLINK_PATH="/usr/local/bin/glit"
+PREFIX_COMMAND=""
+IS_ROOT=false
 UNATTENDED=false
+IS_INPUT_INTERACTIVE=false
+IS_OUTPUT_INTERACTIVE=false
+OS_NAME="$(uname)"
+MINIMUM_BASH_VERSION=4.2
+INSTALLED_BASH_VERSION="${BASH_VERSINFO[0]}.${BASH_VERSINFO[1]}"
+BASH_VERSION_ISSUE=false
+MISSING_REQUIRED_PKGS=()
+MISSING_RECCOMENDED_PKGS=()
 
-# Argument parsing
+declare -A DEPENDENCIES=(
+    ["curl"]="required"
+    ["git"]="required"
+    ["rsync"]="required"
+    ["uname"]="required"
+    ["realpath"]="required"
+    ["bc"]="required"
+    ["tput"]="recommended"
+)
+
+declare -A DEP_TO_PKG_MAP=(
+    ["curl"]="curl"
+    ["git"]="git"
+    ["rsync"]="rsync"
+    ["uname"]="coreutils"
+    ["realpath"]="coreutils"
+    ["tput"]="ncurses"
+    ["bash"]="bash"
+    ["bc"]="bc"
+)
+
+# Set conditional values
+(( $(id -u) == 0 )) && IS_ROOT=true || :
+[[ -t 0 ]] && IS_INPUT_INTERACTIVE=true || :
+[[ -t 1 ]] && IS_OUTPUT_INTERACTIVE=true || :
+[[ ! $IS_ROOT ]] && PREFIX_COMMAND="sudo " || :
+[[ "$OS_NAME" == "Linux" && -f "/etc/debian_version" ]] && DEP_TO_PKG_MAP["tput"]="ncurses-bin" || :
+[[ "$INSTALL_MODE" == "remote" ]] && TEMP_DIR=$(mktemp -d) || :
+
+cleanup() {
+    echo -e "\nInstallation aborted. Cleaning up temporary files...\n"
+    rm -rf "$TEMP_DIR"
+
+    exit 1
+}
+
+# NOTE: Only trap on ERR, INT and TERM signals. TEMP_DIR is explicitly removed on successful EXIT.
+trap cleanup ERR INT TERM
+
+# Check Bash version
+if (( $(echo "$INSTALLED_BASH_VERSION < $MINIMUM_BASH_VERSION" | bc -l) )); then
+    BASH_VERSION_ISSUE=true
+    MISSING_REQUIRED_PKGS+=("${DEP_TO_PKG_MAP["bash"]}")
+fi
+
+# Check for missing dependencies
+for DEPENDENCY in "${!DEPENDENCIES[@]}"; do
+    command -v "$DEPENDENCY" &> /dev/null && continue
+
+    if [[ "${DEPENDENCIES[$DEPENDENCY]}" == "required" ]]; then
+        MISSING_REQUIRED_PKGS+=("${DEP_TO_PKG_MAP["$DEPENDENCY"]}")
+    else
+        MISSING_RECCOMENDED_PKGS+=("${DEP_TO_PKG_MAP["$DEPENDENCY"]}")
+    fi
+done
+
+# Quick sanity check on the following variables as we rm -rf them later.
+# NOTE: This is just for belt and braces. The user is unable to set these variables.
+if [[ "$GLIT_DIR" == "/" ]] || [[ "$TEMP_DIR" == "/" ]]; then
+    echo -e "\nInvalid value for GLIT_DIR or TEMP_DIR. Aborting."
+
+    exit 1
+fi
+
+# --- Argument Parsing ---
+
 while (( "$#" )); do
   case "$1" in
     local)
@@ -35,38 +114,13 @@ while (( "$#" )); do
   esac
 done
 
-GH_API_ENDPOINT="https://api.github.com/repos/justJackjon/glit"
-GLIT_REPO_URL="https://github.com/justJackjon/glit"
-GLIT_DIR="/opt/glit"
-SYMLINK_PATH="/usr/local/bin/glit"
-DEPENDENCIES=("curl" "git" "rsync" "uname" "realpath")
-
-if [[ "$INSTALL_MODE" == "remote" ]]; then
-    TEMP_DIR=$(mktemp -d)
-fi
-
-# Quick sanity check on the following variables as we rm -rf them later.
-if [[ "$GLIT_DIR" == "/" ]] || [[ "$TEMP_DIR" == "/" ]]; then
-    echo -e "\nInvalid value for GLIT_DIR or TEMP_DIR. Aborting."
-
-    exit 1
-fi
-
-cleanup() {
-    echo -e "\n\nInstallation aborted. Cleaning up temporary files..."
-    rm -rf "$TEMP_DIR"
-
-    exit 1
-}
-
-# NOTE: Only trap on ERR, INT and TERM signals. TEMP_DIR is explicitly removed on successful EXIT.
-trap cleanup ERR INT TERM
 
 HEAVY_CHECK_MARK="\u2714"
 RED="\e[31m"
 GREEN="\e[32m"
 BLUE="\e[34m"
 RESET="\e[0m"
+# --- Fn Declarations ---
 
 print() {
     local message_type="$1"
@@ -81,23 +135,55 @@ print() {
         info) color="$BLUE";;
     esac
 
-    if [[ -t 1 ]]; then
+    if $IS_OUTPUT_INTERACTIVE; then
         echo -e "${color}\n${prefix}${message}${suffix}$RESET"
     else
         echo -e "\n${prefix}${message}${suffix}"
     fi
 }
 
+print_dependencies() {
+    local message_type=$1; shift
+    local message=$1; shift
+    local dependencies=("$@")
+
+    print "$message_type" "$message\n"
+
+    for dependency in "${dependencies[@]}"; do
+        echo " - $dependency"
+    done
+}
+
+provide_os_advice() {
+    local os_message=$1
+    local command_prefix=$2
+    local command_name=$3
+    local install_command=$4
+    local extra_command=$5
+    local additional_info=$6
+    local deps_space=""
+
+    (( ${#MISSING_REQUIRED_PKGS[@]} > 0 && ${#MISSING_RECCOMENDED_PKGS[@]} > 0 )) && deps_space=" " || :
+
+    print info "It seems you're using $os_message. You can install these dependencies using $command_name:\n"
+
+    [[ ! -z "$extra_command" ]] && echo -e "  \`${command_prefix}${extra_command}\`" || :
+
+    echo -e "  \`${command_prefix}${install_command} ${MISSING_REQUIRED_PKGS[*]-}${deps_space}${MISSING_RECCOMENDED_PKGS[*]-}\`\n"
+
+    [[ ! -z "$additional_info" ]] && echo -e "$additional_info\n" || :
+}
+
 ask_should_reinstall() {
     print info "\`glit\` is already installed."
 
-    if $UNATTENDED || [[ ! -t 0 ]]; then
+    if $UNATTENDED || ! $IS_INPUT_INTERACTIVE; then
         print info "Running in non-interactive mode. Assuming 'yes' for reinstall."
 
         return 0
     fi
 
-    echo ""
+    echo
     read -p "Do you want to reinstall it? [y/N]: " response
 
     if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]; then
@@ -107,14 +193,69 @@ ask_should_reinstall() {
     fi
 }
 
-# Check for dependencies
-for dep in "${DEPENDENCIES[@]}"; do
-    if ! command -v "$dep" &> /dev/null; then
-        print error "$dep is not installed. Please install $dep and try again."
+# --- Dependency Checks ---
+
+if
+    $BASH_VERSION_ISSUE || \
+    (( ${#MISSING_REQUIRED_PKGS[@]} > 0 )) || \
+    (( ${#MISSING_RECCOMENDED_PKGS[@]} > 0 ))
+then
+    SHOULD_ABORT=false
+
+    echo -e "\n!!!--------------------------------------------"
+
+    if $BASH_VERSION_ISSUE; then
+        print error "Bash verion $INSTALLED_BASH_VERSION.x is installed."
+        echo
+        echo "The minimum version requred by \`glit\` is $MINIMUM_BASH_VERSION.x."
+        echo "Please upgrade Bash and try again."
+
+        SHOULD_ABORT=true
+    fi
+
+    if (( ${#MISSING_REQUIRED_PKGS[@]} > 0 )); then
+        print_dependencies "error" "Missing or unsupported dependencies:" "${MISSING_REQUIRED_PKGS[@]}"
+
+        SHOULD_ABORT=true
+    fi
+
+    if (( ${#MISSING_RECCOMENDED_PKGS[@]} > 0 )); then
+        print_dependencies "warning" "\`glit\` will work best if you install the following recommended dependencies:" "${MISSING_RECCOMENDED_PKGS[@]}"
+    fi
+
+    echo -e "\n--------------------------------------------!!!"
+
+    case $OS_NAME in
+        Darwin)
+            provide_os_advice "macOS" "$PREFIX_COMMAND" "Homebrew" "brew install" "" \
+            "If you don't have Homebrew yet, visit https://brew.sh to get started."
+            ;;
+        Linux)
+            if [[ -f /etc/debian_version ]]; then
+                provide_os_advice "a Debian-based system" "$PREFIX_COMMAND" "apt" "apt install" "apt update" ""
+            elif [[ -f /etc/redhat-release ]]; then
+                provide_os_advice "a RedHat-based system" "$PREFIX_COMMAND" "yum or dnf" "yum install" "" ""
+            elif [[ -f /etc/alpine-release ]]; then
+                provide_os_advice "Alpine Linux" "$PREFIX_COMMAND" "apk" "apk add" "" ""
+            fi
+            ;;
+        *)
+            echo -e "Please install these dependencies using your system's package manager."
+            ;;
+    esac
+
+    ! $IS_ROOT && echo -e "INFO: Only use \`sudo\` when necessary and understand the risks.\n" || :
+
+    echo -e "!!!--------------------------------------------"
+
+    if $SHOULD_ABORT; then
+        print error "Installation aborted due to unmet prerequisites."
 
         exit 1
     fi
-done
+fi
+
+# --- Installation ---
 
 # Check if the user has write permissions for the install directories
 if [[ ! -w "/opt" || ! -w "/usr/local/bin" ]]; then
